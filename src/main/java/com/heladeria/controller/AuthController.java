f1fd168991f219d35413fb6b8352e1c2dd843bb3
Antonhy <antonhypalmacconislla11@gmail.com>
2026-07-08 21:38:44 -0500

    Dejando en Proyecto Final Funcional

--- src/main/java/com/heladeria/controller/AuthController.java
+++ src/main/java/com/heladeria/controller/AuthController.java
@@ -1,31 +1,41 @@
 package com.heladeria.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.heladeria.dto.LoginRequest;
 import com.heladeria.dto.LoginResponse;
-import com.heladeria.security.JwtService;
-
+import com.heladeria.model.Usuario;
+import com.heladeria.service.AuthService;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
-    private JwtService jwtService;
+    private AuthService authService;
 
+    // ==========================
+    // REGISTRAR USUARIO
+    // ==========================
+    @PostMapping("/register")
+    public Usuario register(@RequestBody Usuario usuario) {
+
+        return authService.register(usuario);
+
+    }
+
+    // ==========================
+    // LOGIN
+    // ==========================
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest request) {
 
-        // Usuario de prueba
-        if ("admin".equals(request.getUsername())
-                && "123456".equals(request.getPassword())) {
+        return authService.login(
+                request.getUsername(),
+                request.getPassword());
 
-            String token = jwtService.generateToken(request.getUsername());
-
-            return new LoginResponse(token);
-        }
-
-        throw new RuntimeException("Usuario o contraseña incorrectos");
     }
+
 }
--- src/main/java/com/heladeria/model/Usuario.java
+++ src/main/java/com/heladeria/model/Usuario.java
@@ -3,29 +3,19 @@ package com.heladeria.model;
 
 import jakarta.persistence.*;
 
-
-
 @Entity
 @Table(name="usuarios")
 public class Usuario {
-
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
-
     @Column(unique=true, nullable=false)
     private String username;
 
-
-
     @Column(nullable=false)
     private String password;
-
-
 
     private String rol;
 
